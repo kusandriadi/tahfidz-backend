19c30cda30ed19a2f18a1b274c4fe3243853726a
Kus Andriadi <me@kusandriadi.com>
2022-07-12 12:29:03 +0700

    update database

--- service/database.go
+++ service/database.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ConnectToDatabase() *gorm.DB {
-	dsn := "root:password@tcp(103.189.234.120:3306)/tunasmulia?loc=Asia%2FJakarta&charset=utf8&parseTime=True"
+	dsn := "root:@tcp(localhost:3306)/tunasmulia?loc=Asia%2FJakarta&charset=utf8&parseTime=True"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB Connection Error " + err.Error())
